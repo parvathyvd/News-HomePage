4d3c9b48062172fbcd659d281d977129cfe38566
Parvathy VD <paru@Parvathys-MacBook-Pro.local>
2022-10-28 13:03:49 -0700

    js clean up

--- index.js
+++ index.js
@@ -17,31 +17,3 @@ const closeMenu = () => {
 open.addEventListener("click", toggleMenu);
 close.addEventListener("click", closeMenu);
 backDrop.addEventListener("click", closeMenu);
-
-var a = "test";
-var b = String("test");
-var c = new String("test");
-
-console.log(b == c);
-
-var a = [99, 87, , 9];
-a.find(function (value, index) {
-  if (index == 2) {
-    delete a[2];
-  }
-  console.log(index);
-});
-
-let propBar = "bar1";
-let b2 = "bar2";
-
-class Class1 {
-  set [propBar](val) {
-    this.value = val;
-  }
-}
-let class2 = new Class1();
-console.log(class2.value);
-
-let z = 24 / 3 + 12 / 4 - 7;
-console.log(z);
